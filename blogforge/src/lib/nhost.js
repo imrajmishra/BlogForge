034f5503e2295a56f8b7acde0c6f1b54e7615c4d
Raj Mishra <raj212558@gmail.com>
2026-06-21 18:09:52 +0530

    fix: update nhost config

--- blogforge/src/lib/nhost.js
+++ blogforge/src/lib/nhost.js
@@ -2,6 +2,6 @@ import { createClient } from "@nhost/nhost-js";
 
 // Replace <subdomain> and <region> with your project's values
 export const nhost = createClient({
-  subdomain: "<subdomain>",
-  region: "<region>",
+  subdomain: process.env.NEXT_NHOST_SUBDOMAIN,
+  region: process.env.NEXT_NHOST_REGION,
 });
